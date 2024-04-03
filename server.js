3461e30e55f98930db158d5812fef876dc0dd0a7
amitnarware <amitnarware40@gmail.com>
2024-04-03 15:22:09 +0530

    again port change

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 
 app.use(routes);
 
-app.listen(4000, () => {
-      console.log("Server is running on port 4000");
+app.listen(5000, () => {
+      console.log("Server is running on port 5000");
     });
     // https://code.pieces.app/blog/role-based-access-systems-in-nodejs
